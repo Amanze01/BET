7de58ede591d8acfc984486f0721190d462bb132
Henry <odunzeamanze@gmail.com>
2023-05-29 15:41:53 -0400

    Update TwoSum.cs

--- TwoSum.cs
+++ TwoSum.cs
@@ -1,1 +1,27 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        
+        var dictMap = new Dictionary<int, int>();
+        int[] toReturn = null;
 
+        for (int i = 0; i < nums.Length; i++){
+            int x = nums[i];
+            int y = target - x;
+            
+            if (dictMap.ContainsKey(y)){
+                toReturn = new int[] {dictMap[y], i};
+            }
+            dictMap[x] = i;         
+        }
+        string[] result = Array.
+        ConvertAll(toReturn, x => x.ToString());
+
+        Console.WriteLine("[" + String.Join(",", result) + "]");
+
+        return toReturn;
+    }      
+}
+
+//Example
+//Given [2,7,11,15], target = 9
+//Return indices [0,1]
